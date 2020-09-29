726e9a4dd9acb2586f870ea43efd76e0776481c9
Olivia Chisman <olivia.chisman@gmail.com>
2020-09-28 21:40:35 -0500

    mi to km working

--- app/src/main/java/com/oliviarojas/distanceconverter/MainActivity.java
+++ app/src/main/java/com/oliviarojas/distanceconverter/MainActivity.java
@@ -3,12 +3,52 @@ package com.oliviarojas.distanceconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputValue;
+    private TextView outputValue;
+    private TextView historyValue;
+    private DecimalFormat format = new DecimalFormat("#.#");
+
+    private static final String TAG = "MainActivityTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputValue = findViewById(R.id.inputValue);
+        outputValue = findViewById(R.id.resultValue);
+        historyValue = findViewById(R.id.conversionHistoryText);
+    }
+
+    public void convertButtonClicked(View view) {
+        String inputString = inputValue.getText().toString();
+        Log.d(TAG, "Got string value: " + inputString);
+        if (inputString == null || inputString.isEmpty()) {
+            return;
+        }
+        Double input = Double.valueOf(inputString);
+        String output = format.format(input * 1.60934);
+
+        outputValue.setText(output);
+        if (historyValue.getText().toString().isEmpty()) {
+            historyValue.setText(output);
+        } else {
+            StringBuilder builder = new StringBuilder(output);
+            builder.append("\n").append(historyValue.getText().toString());
+            historyValue.setText(builder.toString());
+        }
+    }
+
+    public void clearButtonClicked(View view) {
+        historyValue.setText("");
     }
 }
