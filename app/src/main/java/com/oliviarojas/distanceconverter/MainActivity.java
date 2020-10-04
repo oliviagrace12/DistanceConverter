61f48c0138aa4448dae6545f0a3ac7bd8d7f6fbd
Olivia Chisman <olivia.chisman@gmail.com>
2020-10-04 14:25:00 -0500

    done

--- app/src/main/java/com/oliviarojas/distanceconverter/MainActivity.java
+++ app/src/main/java/com/oliviarojas/distanceconverter/MainActivity.java
@@ -1,10 +1,10 @@
 package com.oliviarojas.distanceconverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView inputLabel;
     private EditText inputValue;
-    private TextView outputLabel;
-    private TextView outputValue;
+    private TextView resultLabel;
+    private TextView resultValue;
     private TextView historyValue;
     private DecimalFormat format = new DecimalFormat("#.#");
     private boolean isMiToKm = true;
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         inputLabel = findViewById(R.id.inputLabel);
         inputValue = findViewById(R.id.inputValue);
-        outputLabel = findViewById(R.id.resultLabel);
-        outputValue = findViewById(R.id.resultValue);
+        resultLabel = findViewById(R.id.resultLabel);
+        resultValue = findViewById(R.id.resultValue);
         historyValue = findViewById(R.id.conversionHistoryText);
         historyValue.setMovementMethod(new ScrollingMovementMethod());
     }
@@ -39,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
     public void convertButtonClicked(View view) {
         String input = inputValue.getText().toString();
         if (input.isEmpty()) {
-            outputValue.setText("");
+            resultValue.setText("");
             return;
         }
 
         String output = convert(input);
-        outputValue.setText(output);
+        resultValue.setText(output);
         setHistory(input, output);
     }
 
@@ -80,11 +80,25 @@ public class MainActivity extends AppCompatActivity {
         if (R.id.milesToKilometers == view.getId()) {
             isMiToKm = true;
             inputLabel.setText(R.string.miles_value);
-            outputLabel.setText(R.string.kilometers_value);
+            resultLabel.setText(R.string.kilometers_value);
         } else {
             isMiToKm = false;
             inputLabel.setText(R.string.kilometers_value);
-            outputLabel.setText(R.string.miles_value);
+            resultLabel.setText(R.string.miles_value);
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(getString(R.string.history_state), historyValue.getText());
+        outState.putCharSequence(getString(R.string.result_state), resultValue.getText());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        historyValue.setText(savedInstanceState.getCharSequence(getString(R.string.history_state)));
+        resultValue.setText(savedInstanceState.getCharSequence(getString(R.string.result_state)));
     }
 }
